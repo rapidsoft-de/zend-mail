93e4feccf0a44823a1d6cf702f02afe350c30ff9
Jan-Simon Winkelmann <jwinkelmann@rapidsoft.de>
2015-10-26 11:55:19 +0100

    Revert strip_comments handling in abstract address list as it also removes them from within quoted strings which is not allowed as of RFC5322#2.2.3

--- src/Header/AbstractAddressList.php
+++ src/Header/AbstractAddressList.php
@@ -64,7 +64,6 @@ abstract class AbstractAddressList implements HeaderInterface
             $values,
             function (&$value) {
                 $value = trim($value);
-                $value = self::stripComments($value);
             }
         );
         $values = array_filter($values);
@@ -157,20 +156,5 @@ abstract class AbstractAddressList implements HeaderInterface
         $name  = $this->getFieldName();
         $value = $this->getFieldValue(HeaderInterface::FORMAT_ENCODED);
         return (empty($value)) ? '' : sprintf('%s: %s', $name, $value);
-    }
-
-    // Supposed to be private, protected as a workaround for PHP bug 68194
-    protected static function stripComments($value)
-    {
-        return preg_replace(
-            '/\\(
-                (
-                    \\\\.|
-                    [^\\\\)]
-                )+
-            \\)/x',
-            '',
-            $value
-        );
     }
 }
